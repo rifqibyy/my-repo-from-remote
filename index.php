75c2046af7e9a322f25ccf6c20f9d8c7736e6338
rifqibyy <rifqibyy@gmail.com>
2019-04-19 07:31:15 +0700

    added style for class header

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        .header {
+            width: 100%;
+            height: 50px;
+            line-hight: 50px;
+            background-color: burlywood;
+            text-align: center;
+            color: white;
+        }
+    </style>
 </head>
 <body>
-    Hello, World!
+<div class="header">Header</div>
+    <div>
+        <img src="<?php echo "https://www.gravatar.com/avatar/" . md5( strtolower( trim( 'rifqibyy@gmail.com' ) ) ) ?>" alt="Profile Picture">
+        <h2>Rifqi Abiyyu</h2>
+    </div>
 </body>
 </html>
